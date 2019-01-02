1f36e2197c4cf5e1f66e937069db6d870ad04caa
Jonathan.Cohen <jonathan.cohen@usta.com>
2019-01-02 14:49:18 -0500

    added setup.py and moved to spring-struct package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='spring-struct',
+   version='1.0',
+   description='Creates Spring directories under root package',
+   author='J. Cohen',
+   author_email='jcohen66@optonline.net',
+   packages=['spring-struct'],  #same as name
+   install_requires=[], #external packages as dependencies
+)
